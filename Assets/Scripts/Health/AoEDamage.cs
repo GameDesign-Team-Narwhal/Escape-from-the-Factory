a9c75d36dc1f2db5e275f5a4956577ce26f76074
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2016-04-27 13:12:09 -0700

    Add damage scripts

--- Assets/Scripts/Health/AoEDamage.cs
+++ Assets/Scripts/Health/AoEDamage.cs
@@ -1,15 +1,47 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class AoEDamage : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public int damageAmount;
+	public float damageRadius;
+
+	public string team = "";
+
+	AoEDamageSource damageSource;
+
+	void Awake()
+	{
+		damageSource = new AoEDamageSource(this);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void DoDamage()
+	{
+		Collider[] foundColliders = Physics.OverlapSphere(transform.position, damageRadius);
+
+		foreach(Collider collider in foundColliders)
+		{
+			List<IHealthEntity> healthEntities = Utils.GetBehaviorsWithInterface<IHealthEntity>(collider.gameObject);
+			foreach(IHealthEntity entity in healthEntities)
+			{
+				entity.Damage(team, damageSource, damageAmount);
+			}
+		}
 	}
+
+	public class AoEDamageSource : DamageSource
+	{
+		AoEDamage damager;
+
+		public AoEDamageSource(AoEDamage damager)
+		{
+			this.damager = damager;
+		}
+
+		public override string GetName()
+		{
+			return "AoE Damage";
+		}
+	
+	} 
 }
--- Assets/Scripts/Health/ContactDamage.cs
+++ Assets/Scripts/Health/ContactDamage.cs
@@ -2,36 +2,148 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class ContactDamage : ObjectCounter {
+public class ContactDamage : MonoBehaviour {
 
 	public string team = "Enemies";
 
-	public float damageAmount;
+	public int damageAmount;
+	public bool continuousDamage;
+	public bool initialDamage;
+	public float continuousDamagePeriod; //sec
 
-	public bool continuousDamage = false;
-	public float continuousDamagePeriod = 1; //sec
+	public Dictionary<IHealthEntity, uint> entitiesInsideCount = new Dictionary<IHealthEntity, uint>();
+	public Dictionary<IHealthEntity, float> nextDamageTimes = new Dictionary<IHealthEntity, float>();
 
-	private Dictionary<HealthEntity, float> damageCooldowns;
 
-	public void OnTriggerEnter(Collider other)
+	private ContactDamageSource damageSource;
+
+	public float knockback = 0;
+
+	void Awake()
 	{
-		HealthEntity healthEntity = other.GetComponent<HealthEntity>();
-		if(healthEntity != null && !healthEntity.IsOnTeam(team))
-		{
-			healthEntity.Damage(damageAmount);
+		damageSource = new ContactDamageSource();
+	}
 
-			if(continuousDamage)
+	public void OnTriggerEnter(Collider collider)
+	{
+
+		IHealthEntity healthEntity = Utils.GetBehaviorWithInterface<IHealthEntity>(collider.gameObject);
+		if(healthEntity != null)
+		{
+
+			IHealthEntity parent = healthEntity.GetParent();
+
+			IHealthEntity entityToReferTo = parent == null ? healthEntity : parent; 
+
+			if(entitiesInsideCount.ContainsKey(entityToReferTo))
 			{
-				damageCooldowns.Add(healthEntity, continuousDamagePeriod);
+				//already inside
+				uint prevCount = entitiesInsideCount[entityToReferTo];
+				entitiesInsideCount[entityToReferTo] = prevCount + 1;
+			}
+			else
+			{
+
+				entitiesInsideCount.Add(entityToReferTo, 1);
+
+				if(continuousDamage)
+				{
+					nextDamageTimes.Add(entityToReferTo, Time.time + continuousDamagePeriod);
+				}
+			}
+
+			if(initialDamage)
+			{
+				healthEntity.Damage(team, damageSource, damageAmount);
+
+					
+				DealKnockback(collider.attachedRigidbody);
 			}
 		}
 
 
 	}
 
-	public static class EntityDamageSource : DamageSource
+	/**
+	 * 
+	 * Deals knockback to a rigidbody according to how the class is set.
+	 * Does nothing if the rigidbody is null.
+	*/
+	void DealKnockback(Rigidbody body)
+	{
+		//knockback
+		if(body != null)
+		{
+			Vector3 deltaPos = body.gameObject.transform.position - transform.position;
+			Vector3 knockbackImpulse = deltaPos.normalized * knockback;
+			
+			body.AddForce(knockbackImpulse, ForceMode.Impulse);
+		}
+	}
+
+	void FixedUpdate()
+	{
+		//apparently modifying a dictionary while iterating through it is an unsolved proplem in c#
+		Dictionary<IHealthEntity, float> newDamageTimes = new Dictionary<IHealthEntity, float>(nextDamageTimes);
+
+		foreach(KeyValuePair<IHealthEntity, float> entityTime in nextDamageTimes)
+		{
+
+			if(entityTime.Value < Time.time)
+			{
+				entityTime.Key.Damage(team, damageSource, damageAmount);
+				DealKnockback(entityTime.Key.GetGameObject().GetComponent<Rigidbody>());
+				newDamageTimes[entityTime.Key] = Time.time + continuousDamagePeriod;
+			}
+			if(!entityTime.Key.IsAlive())
+			{
+				newDamageTimes.Remove(entityTime.Key);
+				entitiesInsideCount.Remove(entityTime.Key);
+			}
+
+		}
+
+		nextDamageTimes = newDamageTimes;
+
+		Debug.Log("counts: " + Utils.DictionaryToString(entitiesInsideCount) + " times: " + Utils.DictionaryToString(nextDamageTimes));
+	}
+
+	public void OnTriggerExit(Collider collider)
+	{
+
+		IHealthEntity healthEntity = Utils.GetBehaviorWithInterface<IHealthEntity>(collider.gameObject);
+		if(healthEntity != null)
+		{
+			IHealthEntity parent = healthEntity.GetParent();
+			
+			IHealthEntity entityToReferTo = parent == null ? healthEntity : parent; 
+			
+			if(entitiesInsideCount.ContainsKey(entityToReferTo))
+			{
+				//already inside
+				uint prevCount = entitiesInsideCount[entityToReferTo];
+				prevCount -= 1;
+				if(prevCount == 0)
+				{
+					//all of the parts of the entity are out of the area, remove them.
+					entitiesInsideCount.Remove(entityToReferTo);
+					nextDamageTimes.Remove(entityToReferTo);
+				}
+				else
+				{
+					entitiesInsideCount[entityToReferTo] = prevCount;
+				}
+			}
+			
+		}
+		
+	}
+
+
+	public class ContactDamageSource : DamageSource
 	{	
-		public string GetName()
+
+		public override string GetName()
 		{
 			return "Contact Damage";
 		}
